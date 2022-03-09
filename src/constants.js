a934c226da97b8df8c0a5783a7531bc4ca726e89
peteryoungy <49567972+peteryoungy@users.noreply.github.com>
2022-03-08 21:36:37 -0500

    change backend server url

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const TOKEN_KEY = 'token';
 // export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
 
-export const BASE_URL = 'http://34.136.122.242:8080';
+export const BASE_URL = 'https://molten-nirvana-340903.ue.r.appspot.com';
 
 export const SEARCH_KEY = {
     all: 0,
